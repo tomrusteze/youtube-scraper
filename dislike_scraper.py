94ef823e9187400cafa8082d0b8f4548fff4a8fa
Tom Rust <tomrusteze@gmail.com>
2022-12-15 18:03:03 +0100

    Add dislike scraper

---
+++ dislike_scraper.py
@@ -0,0 +1,32 @@
+"""
+By Tom Rust
+GitHub : https://github.com/tomrusteze
+"""
+
+import pandas as pd
+import requests, sys, time
+
+SLEEP = 1
+
+
+def get_dislikes(id, title):
+    request = requests.get(f"https://returnyoutubedislikeapi.com/votes?videoId={id}", timeout=5)
+    try:
+        dislikes = request.json()["dislikes"]
+    except:
+        dislikes = 0
+    print("Got", dislikes ,"dislikes for", title)
+    time.sleep(SLEEP)
+    return dislikes
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} videos.csv")
+        exit()
+
+    FILE_NAME = sys.argv[1]
+    
+    df_video = pd.read_csv(FILE_NAME)
+    df_video['dislikes'] = df_video.apply(lambda row: get_dislikes(row['video_id'], row['title']), axis = 1)
+    df_video.to_csv(f"{FILE_NAME}", encoding='utf-8', index=False)    
--- scrape.sh
+++ scrape.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-COMMENT_LIMIT=1000
+COMMENT_LIMIT=100
 
 # Scrape the most popular videos at the moment
 python3 video_scraper.py --output_dir data/scraper
@@ -8,9 +8,8 @@ python3 video_scraper.py --output_dir data/scraper
 #Scrape the comments belonging to these features
 for file in data/scraper/*
 do	
+	echo "Running dislike_scraper.py on $file"
+	python3 dislike_scraper.py $file
 	echo "Running comment_scraper.py on $file"
 	python3 comment_scraper.py $file $COMMENT_LIMIT
 done
-
-
-#python3 comment_scraper.py 
