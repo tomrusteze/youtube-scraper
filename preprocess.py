4d21a8ad37db24ec027377b0171fee07e257987b
Tom Rust <tomrusteze@gmail.com>
2022-12-19 18:43:21 +0100

    Preprocess a single dataset and add flair

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,9 @@ from pattern.en import parse
 from pattern.en import pprint
 from pattern.en import sentiment
 
+from flair.models import TextClassifier
+from flair.data import Sentence
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -30,23 +33,21 @@ nltk.download('vader_lexicon')
 def tokenize_comment(sentence):
     return ' '.join([wnl.lemmatize(word) for word in sentence.split() if word not in set(stopwords.words('english'))])
 
+def flair_prediction(x):
+    sentence = Sentence(x)
+    sia.predict(sentence)
+    return sentence.score
+
 
 if __name__ == '__main__':
     
-    sentiment_library = "Pattern" #"NLTK"
+    sentiment_library = "Pattern" #"NLTK" #Flair
     output_dir = "data/youtube_comment/preprocessed"
     comments_1_file = 'data/youtube_comment/UScomments_1.csv'
     videos_1_file = 'data/youtube_comment/USvideos_1.csv'
-    comments_2_file = 'data/youtube_comment/UScomments_2.csv'
-    videos_2_file = 'data/youtube_comment/USvideos_2.csv'
 
-    #With original dislikes dataset
     comments_1 = pd.read_csv(comments_1_file, error_bad_lines=False)
     videos_1 = pd.read_csv(videos_1_file, error_bad_lines=False)
-
-    #With Return YouTube dislikes on new videos
-    comments_2 = pd.read_csv(comments_2_file, error_bad_lines=False)
-    videos_2 = pd.read_csv(videos_2_file, error_bad_lines=False)
 
 
     print("Old dataset videos shape:", videos_1.shape, "and comments:", comments_1.shape)
@@ -58,12 +59,7 @@ if __name__ == '__main__':
         comments_1['Sentiment Scores'] = comments_1['comment_text'].apply(lambda x: sentiment(x))
         comments_1['Sentiment Subjectivity'] = comments_1['Sentiment Scores'].apply(lambda x: x[1])
         comments_1['Sentiment Scores'] = comments_1['Sentiment Scores'].apply(lambda x: x[0])
-
-        # Dataset 2
-        comments_2['Sentiment Scores'] = comments_2['comment_text'].apply(lambda x: sentiment(x))
-        comments_2['Sentiment Subjectivity'] = comments_2['Sentiment Scores'].apply(lambda x: x[1])
-        comments_2['Sentiment Scores'] = comments_2['Sentiment Scores'].apply(lambda x: x[0])
-
+ 
     # NLTK Library
     if sentiment_library == "NLTK":
         comments_1.dropna(inplace=True)
@@ -74,18 +70,21 @@ if __name__ == '__main__':
         comments_1['comment_text'] = comments_1['comment_text'].str.replace("[^a-zA-Z#]", " ")
         comments_1['comment_text'] = comments_1['comment_text'].apply(lambda x: ' '.join([w for w in x.split() if len(w)>3]))
         comments_1['comment_text'] = comments_1['comment_text'].apply(lambda x:x.lower())
-        
+
+
         wnl = WordNetLemmatizer()
         comments_1['comment_text'] = comments_1['comment_text'].apply(tokenize_comment)
 
         sia = SentimentIntensityAnalyzer()
         comments_1['Sentiment Scores'] = comments_1['comment_text'].apply(lambda x:sia.polarity_scores(x)['compound'])
 
+    # Flair Library
+    if sentiment_library == "Flair":
+        sia = TextClassifier.load('en-sentiment')
+        comments_1["Sentiment Scores"] = comments_1["comment_text"].apply(flair_prediction)
 
     comments_1['Sentiment'] = comments_1['Sentiment Scores'].apply(lambda s : 'Positive' if s > 0 else ('Neutral' if s == 0 else 'Negative'))
-    comments_2['Sentiment'] = comments_2['Sentiment Scores'].apply(lambda s : 'Positive' if s > 0 else ('Neutral' if s == 0 else 'Negative'))
     print("Dataset 1: \n", comments_1.Sentiment.value_counts())
-    print("Dataset 2: \n", comments_2.Sentiment.value_counts())
 
     # Calculate sentiment scores per video on dataset 1
     v_comments = {'nr_comments': [], 'nr_comments_neutral': [], 'nr_comments_positive': [], 'nr_comments_negative': [], 'sentiment': [], 'sentiment_positive': [], 'sentiment_negative': []}
@@ -119,63 +118,21 @@ if __name__ == '__main__':
     videos_1['negative_sentiment_per_comment'] = videos_1['sentiment_negative'] / videos_1['comment_negative']
     videos_1['sentiment_per_non_neutral_comment'] = videos_1['sentiment'] / (videos_1['comment_retrieved'] - videos_1['comment_neutral'])
 
-    # Calculate sentiment scores per video on dataset 2
-    v_comments = {'nr_comments': [], 'nr_comments_neutral': [], 'nr_comments_positive': [], 'nr_comments_negative': [], 'sentiment': [], 'sentiment_positive': [], 'sentiment_negative': []}
-    for v in videos_2.video_id:
-        v_nr_comments = comments_2[comments_2.video_id == v].count()[0]
-        v_nr_comments_neutral = comments_2[(comments_2.video_id == v) & (comments_2.Sentiment == 'Neutral')].count()[0]
-        v_nr_comments_positive = comments_2[(comments_2.video_id == v) & (comments_2.Sentiment == 'Positive')].count()[0]
-        v_nr_comments_negative = comments_2[(comments_2.video_id == v) & (comments_2.Sentiment == 'Negative')].count()[0]
-        v_sentiment_positive = comments_2[(comments_2.video_id == v) & (comments_2.Sentiment == 'Positive')]['Sentiment Scores'].sum()
-        v_sentiment_negative = comments_2[(comments_2.video_id == v) & (comments_2.Sentiment == 'Negative')]['Sentiment Scores'].sum()
-        v_sentiment = comments_2[comments_2.video_id == v]['Sentiment Scores'].sum()
-        v_comments['nr_comments'].append(v_nr_comments)
-        v_comments['nr_comments_neutral'].append(v_nr_comments_neutral)
-        v_comments['nr_comments_positive'].append(v_nr_comments_positive)
-        v_comments['nr_comments_negative'].append(v_nr_comments_negative)
-        v_comments['sentiment'].append(v_sentiment)
-        v_comments['sentiment_positive'].append(v_sentiment_positive)
-        v_comments['sentiment_negative'].append(v_sentiment_negative)
-
-    videos_2['sentiment'] = v_comments['sentiment']
-    videos_2['sentiment_positive'] = v_comments['sentiment_positive']
-    videos_2['sentiment_negative'] = v_comments['sentiment_negative']
-
-    videos_2['comment_retrieved'] = v_comments['nr_comments']
-    videos_2['comment_neutral'] = v_comments['nr_comments_neutral']
-    videos_2['comment_positive'] = v_comments['nr_comments_positive']
-    videos_2['comment_negative'] = v_comments['nr_comments_negative']
-
-    videos_2['sentiment_per_comment'] = videos_2['sentiment'] / videos_2['comment_retrieved']
-    videos_2['positive_sentiment_per_comment'] = videos_2['sentiment_positive'] / videos_2['comment_positive']
-    videos_2['negative_sentiment_per_comment'] = videos_2['sentiment_negative'] / videos_2['comment_negative']
-    videos_2['sentiment_per_non_neutral_comment'] = videos_2['sentiment'] / (videos_2['comment_retrieved'] - videos_2['comment_neutral'])
-
     # Remove videos with less than 500 comments
     videos_1 = videos_1[videos_1.comment_retrieved > 500]
-    videos_2 = videos_2[videos_2.comment_retrieved > 500]
 
     # Remove videos with 0 dislikes (this never happens in practice, so it must be an error)
     videos_1 = videos_1[videos_1.dislikes > 0]
-    videos_2 = videos_2[videos_2.dislikes > 0]
     print("New shape of old dataset:", videos_1.shape)
-    print("New shape of new dataset:", videos_2.shape)
 
     print("Dataset 1 Sentiment scores:")
     print("Average sentiment on dataset per video", videos_1['sentiment'].sum()/videos_1.count()[0])
     print("Average sentiment on dataset per comment", videos_1['sentiment_per_comment'].sum()/videos_1.count()[0])
     print("Average non-neutral sentiment on dataset per video per comment", videos_1['sentiment_per_non_neutral_comment'].sum()/videos_1.count()[0])
     
-    print("Dataset 2 Sentiment scores:")
-    print("Average sentiment on dataset per video", videos_2['sentiment'].sum()/videos_2.count()[0])
-    print("Average sentiment on dataset per comment", videos_2['sentiment_per_comment'].sum()/videos_2.count()[0])
-    print("Average non-neutral sentiment on dataset per video per comment", videos_2['sentiment_per_non_neutral_comment'].sum()/videos_2.count()[0])
-
     # Save to file
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     videos_1.to_csv(f"{output_dir}/videos_1_processed_{sentiment_library}.csv", encoding='utf-8', index=False, quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-    videos_2.to_csv(f"{output_dir}/videos_2_processed_{sentiment_library}.csv", encoding='utf-8', index=False, quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
     comments_1.to_csv(f"{output_dir}/comments_1_processed_{sentiment_library}.csv", encoding='utf-8', index=False, quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-    comments_2.to_csv(f"{output_dir}/comments_2_processed_{sentiment_library}.csv", encoding='utf-8', index=False, quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
